095650d96df73a7d7ac1b68daae2743ee035684b
SatvikPatil <patilsatvik1324@gmail.com>
2026-04-14 21:48:40 +0530

    dummy test api added

--- src/app.js
+++ src/app.js
@@ -16,6 +16,9 @@ app.use(cookieParser());
 /** `
  * Use Routes
  */
+app.get("/",(req,res)=>{
+    res.send("Ledger service is running")
+})
 app.use("/api/auth", authRouter);
 app.use("/api/accounts",accountRouter);
 app.use("/api/transactions",transactionRouter);
